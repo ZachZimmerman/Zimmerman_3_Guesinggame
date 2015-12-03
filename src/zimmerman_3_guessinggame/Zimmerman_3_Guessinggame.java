ef36083568622324e10d22908aa5967df5fbcaea
Zach Zimmerman <zzachary54@gmail.com>
2015-12-03 10:38:57 -0600

    Guessing Game

---
+++ src/zimmerman_3_guessinggame/Zimmerman_3_Guessinggame.java
@@ -0,0 +1,54 @@
+package zimmerman_3_guessinggame;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Zimmerman_3_Guessinggame { //Start of program
+static Scanner sc = new Scanner(System.in);
+static int magicnumber;
+static int userguess;
+static String username;
+static Random randy = new Random();
+static boolean playagain;
+static int counter = 1;
+public static void main(String[] args) { //Start of main class
+    playagain = true;
+    magicnumber = randy.nextInt(10);
+    System.out.println("Hello, would you like to play???");
+    System.out.println("Whats your name?");
+    username = sc.nextLine();
+    System.out.println("Hello " + username + " Let's play a game dude bro!!!");
+     
+    //runs while the condition is true    
+    while (playagain){                                                                                                                                                                                                                      
+        playthegame();
+        counter --;
+        if(counter < 1) {
+            System.out.println("You Suck, Uninstall m8");
+            playagain = false;
+        }
+    }
+    } // End of main class
+    static void playthegame(){
+    if (counter > 1);{
+   System.out.println("trys left " +  ( counter )); 
+   }
+    Scanner userinputnumbers = new Scanner(System.in);
+    System.out.println("Please pick a number 1-10");
+    userguess = sc.nextInt();
+    if (magicnumber == userguess){
+        System.out.println("YOU WIN!!!");
+        playagain = false;
+    } else {
+        if(magicnumber > userguess) {
+            System.out.println("Too low! Try again!");
+            playagain = true;
+        } else {
+        System.out.println("Too high! Try again!");
+        playagain = true;
+    }
+
+   
+    } 
+     
+    }
+} //End of program
